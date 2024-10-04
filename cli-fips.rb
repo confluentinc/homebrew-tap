020dfbddfddfea9d424a052ebcd4622cea379d99
Confluent Semaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-10-04 16:45:49 -0700

    Brew formula update for confluent version v4.6.0 (#89)
    Co-authored-by: goreleaserbot <bot@goreleaser.com>

--- cli-fips.rb
+++ cli-fips.rb
@@ -5,20 +5,20 @@
 class CliFips < Formula
   desc "CLI for Confluent Cloud and Confluent Platform"
   homepage "https://docs.confluent.io/confluent-cli/current/overview.html"
-  version "4.5.0"
+  version "4.6.0"
   depends_on :macos
 
   on_intel do
-    url "https://packages.confluent.io/confluent-cli/archives/4.5.0/confluent-fips_darwin_amd64_disableupdates.tar.gz"
-    sha256 "b8fc81e3971e01ec1fd1072d6c62833fbec2e0f02e999a487c8abaf6b20df4d1"
+    url "https://packages.confluent.io/confluent-cli/archives/4.6.0/confluent-fips_darwin_amd64_disableupdates.tar.gz"
+    sha256 "fe427ed3df3467361d795b138cdf2ceda43aa85bfd3bdb87d4a3b7b1269b5702"
 
     def install
       bin.install "confluent"
     end
   end
   on_arm do
-    url "https://packages.confluent.io/confluent-cli/archives/4.5.0/confluent-fips_darwin_arm64_disableupdates.tar.gz"
-    sha256 "63e0918a72acfdeaa3d061b8f3303515e89d385021739c1fa140093486fe4b7e"
+    url "https://packages.confluent.io/confluent-cli/archives/4.6.0/confluent-fips_darwin_arm64_disableupdates.tar.gz"
+    sha256 "33dde801140a524859de80bb47b0889fa05048e82599a7c67a88e11fa19a8066"
 
     def install
       bin.install "confluent"
